4e6c4796323b43b8af078a65f1c7eeaae1d66455
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-15 01:41:41 +0700

    Minimal files to run Hello World

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+const path = require('path')
+
+module.exports = {
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loaders: ['babel'],
+      }
+    ]
+  }
+}
