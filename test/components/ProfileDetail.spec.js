de57fb4682cc1af7c542e94c231babd130cb2cef
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-15 17:50:46 +0700

    Use enzyme to test

--- test/components/ProfileDetail.spec.js
+++ test/components/ProfileDetail.spec.js
@@ -1,7 +1,21 @@
-import { expect } from 'chai'
+import React from 'react'
+import chai, { expect } from 'chai'
+import { shallow } from 'enzyme'
+import chaiEnzyme from 'chai-enzyme'
+import ProfileDetail from '../../src/components/ProfileDetail'
+
+chai.use(chaiEnzyme())
 
 describe('ProfileDetail', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<ProfileDetail />)
+  })
+
   it('render correct structure', () => {
-    expect(true).to.equal(false)
+    expect(wrapper).to.have.tagName('div')
+    expect(wrapper).to.have.className('detail')
+    expect(wrapper).to.have.className('last-section')
   })
 })
