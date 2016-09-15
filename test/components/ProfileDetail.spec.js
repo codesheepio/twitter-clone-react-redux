342668646ca73b77e588ab2c21a36bb2a7795ada
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-15 18:27:14 +0700

    Test descendants

--- test/components/ProfileDetail.spec.js
+++ test/components/ProfileDetail.spec.js
@@ -13,5 +13,15 @@ describe('ProfileDetail', () => {
     expect(wrapper).to.have.tagName('div')
     expect(wrapper).to.have.className('detail')
     expect(wrapper).to.have.className('last-section')
+
+    expect(wrapper).to.have.exactly(3).descendants('.col')
+
+    const cols = wrapper.find('.col')
+    expect(cols.at(0)).to.have.descendants('.text')
+    expect(cols.at(0)).to.have.descendants('.number')
+    expect(cols.at(1)).to.have.descendants('.text')
+    expect(cols.at(1)).to.have.descendants('.number')
+    expect(cols.at(2)).to.have.descendants('.text')
+    expect(cols.at(2)).to.have.descendants('.number')
   })
 })
