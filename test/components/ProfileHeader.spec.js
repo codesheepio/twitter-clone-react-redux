2351f51352525ee75b9ae3583d21265c99ea3c9f
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-15 19:39:27 +0700

    Add test for ProfileHeader

---
+++ test/components/ProfileHeader.spec.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { expect, shallow } from '../test-helper'
+import ProfileHeader from '../../src/components/ProfileHeader'
+
+describe('ProfileHeader', () => {
+  let wrapper
+
+  beforeEach(() => {
+    wrapper = shallow(<ProfileHeader />)
+  })
+
+  it('renders correct structure', () => {
+    expect(wrapper).to.have.tagName('div')
+    expect(wrapper).to.have.className('header')
+
+    expect(wrapper.find('.name')).to.be.present()
+    expect(wrapper.find('.screen-name')).to.be.present()
+  })
+})
