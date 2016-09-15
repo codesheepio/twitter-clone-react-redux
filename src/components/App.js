bd25bce802c9abc70c6b247f64920cd7d549b72e
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-15 20:25:09 +0700

    Show tweet list

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import Profile from './Profile'
+import TweetList from './TweetList'
 
 class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tweets: [
+        { id: 1, name: 'Arnupharp Viratanapanu', screenName: 'topscores', text: 'Hello World' },
+        { id: 2, name: 'Arnupharp Viratanapanu', screenName: 'topscores', text: 'I am handsome' },
+        { id: 3, name: 'Arnupharp Viratanapanu', screenName: 'topscores', text: 'I like pop music' },
+      ],
+    }
+  }
+
   render() {
     return (
-      <Profile />
+      <div>
+        <Profile />
+        <TweetList tweets={this.state.tweets} />
+      </div>
     )
   }
 }
