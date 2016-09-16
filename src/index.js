91329b8a73c1b7c207704d3de03daac1b3915515
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-16 19:47:27 +0700

    Add router

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
-import App from './components/App'
+import { Router, browserHistory } from 'react-router'
+import { syncHistoryWithStore } from 'react-router-redux'
 import configureStore from './store/configureStore'
+import routes from './routes'
 import './style/main.scss'
 
 const store = configureStore()
+const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
   <Provider store={store} >
-    <App />
+    <Router history={history} routes={routes} />
   </Provider>
 , document.getElementById('root'))
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
+import { routerReducer } from 'react-router-redux'
 import profileReducer from './profile'
 import tweetListReducer from './tweetList'
 
 const rootReducer = combineReducers({
+  routing: routerReducer,
   profile: profileReducer,
   tweetList: tweetListReducer,
 })
