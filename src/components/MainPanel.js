7b084f208077c776674b45073425515f7390ac17
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-20 17:08:17 +0700

    MainPanel is able to display NewTweet

--- src/components/MainPanel.js
+++ src/components/MainPanel.js
@@ -1,14 +1,26 @@
 import React from 'react'
+import NewTweet from './NewTweet'
 import TweetList from '../containers/TweetList'
 
 class MainPanel extends React.Component {
   render() {
+    const newTweet = this.props.enableTweet ? <NewTweet /> : ''
+
     return (
       <div className="main-panel">
+        {newTweet}
         <TweetList />
       </div>
     )
   }
+}
+
+MainPanel.propTypes = {
+  enableTweet: React.PropTypes.bool,
+}
+
+MainPanel.defaultProps = {
+  enableTweet: false,
 }
 
 export default MainPanel
--- test/components/MainPanel.spec.js
+++ test/components/MainPanel.spec.js
@@ -4,18 +4,31 @@ import MainPanel from '../../src/components/MainPanel'
 
 describe('MainPanel', () => {
   let wrapper
+  let props
   let state
 
   beforeEach(() => {
+    props = {
+      enableTweet: false,
+    }
     state = {
       tweetList: [],
     }
-    wrapper = reduxMount(<MainPanel />, state)
+    wrapper = reduxMount(<MainPanel {...props} />, state)
   })
 
   it('renders correct structure', () => {
     expect(wrapper).to.have.tagName('div')
     expect(wrapper).to.have.className('main-panel')
     expect(wrapper).to.have.descendants('TweetList')
+    expect(wrapper).to.not.have.descendants('NewTweet')
+  })
+
+  it('contains NewTweet if enableTweet prop is enable', () => {
+    props = {
+      enableTweet: true,
+    }
+    wrapper = reduxMount(<MainPanel {...props} />, state)
+    expect(wrapper).to.have.descendants('NewTweet')
   })
 })
