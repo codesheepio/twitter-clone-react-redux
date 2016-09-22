8aeebfddf2babc0efbc8f29a4f2a9953f3ab2672
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-22 20:46:09 +0700

    Add mock initial state

--- src/index.js
+++ src/index.js
@@ -9,7 +9,22 @@ import routes from './routes'
 import './style/custom.scss'
 import './style/main.scss'
 
-const store = configureStore()
+const initialState = {
+  profile: {
+    name: 'Arnupharp Viratapanu',
+    screenName: 'topscores',
+    numTweets: 50,
+    numFollowers: 500,
+    numFollowings: 100,
+  },
+  tweetList: [
+    { id: 1, name: 'Supasate Choochaisri', screenName: 'kaizerwing', tweetText: 'Hello World', timestamp: 1474531661204 },
+    { id: 2, name: 'Arnupharp Viratanapanu', screenName: 'topscores', tweetText: 'I am handsome', timestamp: 1474531661207 },
+    { id: 3, name: 'Supasate Choochaisri', screenName: 'kaizerwing', tweetText: 'I love react!', timestamp: 1474531661209 },
+  ],
+}
+
+const store = configureStore(initialState)
 const history = syncHistoryWithStore(browserHistory, store)
 
 ReactDOM.render(
