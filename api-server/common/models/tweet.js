43aa802fdb5fb1ba3ca797687ac30f22c2bad758
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-27 17:17:40 +0700

    Add TwiseterUser model and hide several APIs

--- api-server/common/models/tweet.js
+++ api-server/common/models/tweet.js
@@ -1,3 +1,17 @@
 module.exports = function(Tweet) {
-
+  Tweet.disableRemoteMethod("count", true);
+  Tweet.disableRemoteMethod("exists", true);
+  Tweet.disableRemoteMethod("upsert", true);
+  Tweet.disableRemoteMethod("replaceById", true);
+  Tweet.disableRemoteMethod("updateAttributes", false);
+  Tweet.disableRemoteMethod('__count__accessTokens', false);
+  Tweet.disableRemoteMethod('__create__accessTokens', false);
+  Tweet.disableRemoteMethod('__delete__accessTokens', false);
+  Tweet.disableRemoteMethod('__destroyById__accessTokens', false);
+  Tweet.disableRemoteMethod('__findById__accessTokens', false);
+  Tweet.disableRemoteMethod('__get__accessTokens', false);
+  Tweet.disableRemoteMethod('__updateById__accessTokens', false);
+  Tweet.disableRemoteMethod("createChangeStream", true);
+  Tweet.disableRemoteMethod("replaceOrCreate", true);
+  Tweet.disableRemoteMethod("upsertWithWhere", true);
 };
---
+++ api-server/common/models/twister-user.js
@@ -0,0 +1,17 @@
+module.exports = function(TwisterUser) {
+  TwisterUser.disableRemoteMethod("count", true);
+  TwisterUser.disableRemoteMethod("exists", true);
+  TwisterUser.disableRemoteMethod("upsert", true);
+  TwisterUser.disableRemoteMethod("replaceById", true);
+  TwisterUser.disableRemoteMethod("updateAttributes", false);
+  TwisterUser.disableRemoteMethod('__count__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__create__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__delete__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__destroyById__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__findById__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__get__accessTokens', false);
+  TwisterUser.disableRemoteMethod('__updateById__accessTokens', false);
+  TwisterUser.disableRemoteMethod("createChangeStream", true);
+  TwisterUser.disableRemoteMethod("replaceOrCreate", true);
+  TwisterUser.disableRemoteMethod("upsertWithWhere", true);
+};
