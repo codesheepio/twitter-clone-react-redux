f4df562a192440248bd77b4f33b6bc1879425827
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-28 01:57:28 +0700

    Add Follow model

---
+++ api-server/common/models/follow.js
@@ -0,0 +1,17 @@
+module.exports = function(Follow) {
+  Follow.disableRemoteMethod("count", true);
+  Follow.disableRemoteMethod("exists", true);
+  Follow.disableRemoteMethod("upsert", true);
+  Follow.disableRemoteMethod("replaceById", true);
+  Follow.disableRemoteMethod("updateAttributes", false);
+  Follow.disableRemoteMethod('__count__accessTokens', false);
+  Follow.disableRemoteMethod('__create__accessTokens', false);
+  Follow.disableRemoteMethod('__delete__accessTokens', false);
+  Follow.disableRemoteMethod('__destroyById__accessTokens', false);
+  Follow.disableRemoteMethod('__findById__accessTokens', false);
+  Follow.disableRemoteMethod('__get__accessTokens', false);
+  Follow.disableRemoteMethod('__updateById__accessTokens', false);
+  Follow.disableRemoteMethod("createChangeStream", true);
+  Follow.disableRemoteMethod("replaceOrCreate", true);
+  Follow.disableRemoteMethod("upsertWithWhere", true);
+};
