6b8bd05d141d5879795b6071454e87bde16e1082
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-27 09:48:31 +0700

    Add initial state for fetching status

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import { Provider } from 'react-redux'
 import { ReduxRouter } from 'redux-router'
 import configureStore from './store/configureStore'
 import routes from './routes'
+import { NO_FETCHING } from './constants/fetchingStatus'
 import './style/custom.scss'
 import './style/main.scss'
 
@@ -16,6 +17,7 @@ const initialState = {
     numFollowers: 500,
     numFollowings: 100,
   },
+  fetching: NO_FETCHING,
 }
 
 const store = configureStore(initialState)
