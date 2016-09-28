14d64e715533a61279ae65581e1ebfe798b47c90
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-28 21:20:42 +0700

    Modify login result

--- api-server/common/models/twister-user.js
+++ api-server/common/models/twister-user.js
@@ -14,4 +14,27 @@ module.exports = function(TwisterUser) {
   TwisterUser.disableRemoteMethod('createChangeStream', true);
   TwisterUser.disableRemoteMethod('replaceOrCreate', true);
   TwisterUser.disableRemoteMethod('upsertWithWhere', true);
+
+  TwisterUser.afterRemote('login', function(ctx, user, next) {
+    if (ctx.result) {
+      TwisterUser.findOne({
+        where: {
+          username: user.userId,
+        },
+      })
+      .then(function(userInst) {
+        var modifiedResult = {};
+        modifiedResult.token = ctx.result.id;
+        modifiedResult.ttl = ctx.result.ttl;
+        modifiedResult.created = ctx.result.created;
+        modifiedResult.username = userInst.username;
+        modifiedResult.name = userInst.name;
+
+        ctx.result = modifiedResult;
+        next();
+      });
+    } else {
+      next();
+    }
+  });
 };
