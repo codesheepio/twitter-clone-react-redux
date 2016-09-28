089dd7ddbb1b80f5964b181c94f1c3876f9c4d28
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-28 16:14:36 +0700

    Fix jshint

--- api-server/common/models/tweet.js
+++ api-server/common/models/tweet.js
@@ -1,9 +1,9 @@
 module.exports = function(Tweet) {
-  Tweet.disableRemoteMethod("count", true);
-  Tweet.disableRemoteMethod("exists", true);
-  Tweet.disableRemoteMethod("upsert", true);
-  Tweet.disableRemoteMethod("replaceById", true);
-  Tweet.disableRemoteMethod("updateAttributes", false);
+  Tweet.disableRemoteMethod('count', true);
+  Tweet.disableRemoteMethod('exists', true);
+  Tweet.disableRemoteMethod('upsert', true);
+  Tweet.disableRemoteMethod('replaceById', true);
+  Tweet.disableRemoteMethod('updateAttributes', false);
   Tweet.disableRemoteMethod('__count__accessTokens', false);
   Tweet.disableRemoteMethod('__create__accessTokens', false);
   Tweet.disableRemoteMethod('__delete__accessTokens', false);
@@ -11,7 +11,7 @@ module.exports = function(Tweet) {
   Tweet.disableRemoteMethod('__findById__accessTokens', false);
   Tweet.disableRemoteMethod('__get__accessTokens', false);
   Tweet.disableRemoteMethod('__updateById__accessTokens', false);
-  Tweet.disableRemoteMethod("createChangeStream", true);
-  Tweet.disableRemoteMethod("replaceOrCreate", true);
-  Tweet.disableRemoteMethod("upsertWithWhere", true);
+  Tweet.disableRemoteMethod('createChangeStream', true);
+  Tweet.disableRemoteMethod('replaceOrCreate', true);
+  Tweet.disableRemoteMethod('upsertWithWhere', true);
 };
--- api-server/common/models/twister-user.js
+++ api-server/common/models/twister-user.js
@@ -1,9 +1,9 @@
 module.exports = function(TwisterUser) {
-  TwisterUser.disableRemoteMethod("count", true);
-  TwisterUser.disableRemoteMethod("exists", true);
-  TwisterUser.disableRemoteMethod("upsert", true);
-  TwisterUser.disableRemoteMethod("replaceById", true);
-  TwisterUser.disableRemoteMethod("updateAll", true);
+  TwisterUser.disableRemoteMethod('count', true);
+  TwisterUser.disableRemoteMethod('exists', true);
+  TwisterUser.disableRemoteMethod('upsert', true);
+  TwisterUser.disableRemoteMethod('replaceById', true);
+  TwisterUser.disableRemoteMethod('updateAll', true);
   TwisterUser.disableRemoteMethod('__count__accessTokens', false);
   TwisterUser.disableRemoteMethod('__create__accessTokens', false);
   TwisterUser.disableRemoteMethod('__delete__accessTokens', false);
@@ -11,7 +11,7 @@ module.exports = function(TwisterUser) {
   TwisterUser.disableRemoteMethod('__findById__accessTokens', false);
   TwisterUser.disableRemoteMethod('__get__accessTokens', false);
   TwisterUser.disableRemoteMethod('__updateById__accessTokens', false);
-  TwisterUser.disableRemoteMethod("createChangeStream", true);
-  TwisterUser.disableRemoteMethod("replaceOrCreate", true);
-  TwisterUser.disableRemoteMethod("upsertWithWhere", true);
+  TwisterUser.disableRemoteMethod('createChangeStream', true);
+  TwisterUser.disableRemoteMethod('replaceOrCreate', true);
+  TwisterUser.disableRemoteMethod('upsertWithWhere', true);
 };
