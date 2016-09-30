7c082594f5207baf092539ae573677742c7bceb5
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-30 14:42:37 +0700

    Login via LoginForm

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,41 +1,52 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
+import { connect } from 'react-redux'
+import { login } from '../actions/auth'
 
-const LoginForm = props => (
-  <div className="login-form">
-    <form onSubmit={props.handleSubmit}>
-      <div className="login-label">
-        Log in with your username
-      </div>
-      <div className="form-group">
-        <Field
-          name="username"
-          component="input"
-          type="text"
-          className="form-control"
-          placeholder="Username"
-        />
-      </div>
-      <div className="form-group">
-        <Field
-          name="password"
-          type="password"
-          component="input"
-          className="form-control"
-          placeholder="Password"
-        />
-      </div>
-      <div className="form-group text-right">
-        <button className="btn btn-default">Log in</button>
-      </div>
-    </form>
-  </div>
-)
+const LoginForm = (props) => {
+  const handleFormSubmit = (values) => {
+    props.login(values.username, values.password)
+  }
+
+  return (
+    <div className="login-form">
+      <form onSubmit={props.handleSubmit(handleFormSubmit)}>
+        <div className="login-label">
+          Log in with your username
+        </div>
+        <div className="form-group">
+          <Field
+            name="username"
+            component="input"
+            type="text"
+            className="form-control"
+            placeholder="Username"
+          />
+        </div>
+        <div className="form-group">
+          <Field
+            name="password"
+            type="password"
+            component="input"
+            className="form-control"
+            placeholder="Password"
+          />
+        </div>
+        <div className="form-group text-right">
+          <button className="btn btn-default">Log in</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 LoginForm.propTypes = {
   handleSubmit: React.PropTypes.func.isRequired,
+  login: React.PropTypes.func.isRequired,
 }
 
-export default reduxForm({
+const ReduxLoginForm = reduxForm({
   form: 'login',
 })(LoginForm)
+
+export default connect(null, { login })(ReduxLoginForm)
--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,8 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { push } from 'redux-router'
 import NavBar from '../../containers/NavBar'
 import LoginForm from '../../components/LoginForm'
 
 class Login extends React.Component {
+  componentWillMount() {
+    if (this.props.token) {
+      this.props.redirect('/')
+    }
+  }
+
+  componentWillUpdate(nextProps) {
+    if (nextProps.token) {
+      this.props.redirect('/')
+    }
+  }
+
   render() {
     return (
       <div>
@@ -13,4 +27,17 @@ class Login extends React.Component {
   }
 }
 
-export default Login
+Login.propTypes = {
+  token: React.PropTypes.string,
+  redirect: React.PropTypes.func.isRequired,
+}
+
+const mapStateToProps = state => ({
+  token: state.auth.token,
+})
+
+const mapDispatchToProps = dispatch => ({
+  redirect: url => dispatch(push(url)),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(Login)
