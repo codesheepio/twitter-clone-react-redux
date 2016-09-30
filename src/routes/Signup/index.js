b03c9ff07c944a12c74be8d65cbe8df4e0e41e63
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-30 12:23:00 +0700

    Create Signup page

---
+++ src/routes/Signup/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import NavBar from '../../containers/NavBar'
+import SignupForm from '../../components/SignupForm'
+
+const Signup = () =>
+  <div>
+    <NavBar />
+    <SignupForm />
+  </div>
+
+export default Signup
