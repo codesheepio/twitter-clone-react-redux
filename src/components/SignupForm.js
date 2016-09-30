dad32579a357e9e5d02350cb7bf901620780b234
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-30 15:43:16 +0700

    Can signup via SignupForm

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,22 +1,44 @@
 import React from 'react'
+import { connect } from 'react-redux'
 import { reduxForm, Field } from 'redux-form'
+import { signup } from '../actions/auth'
 
-// eslint-disable-next-line react/prop-types
-const FormInput = ({ input, type, placeholder }) =>
-  <div className="form-group">
-    <input className="form-control" {...input} type={type} placeholder={placeholder} />
-  </div>
 
-const SignupForm = () =>
-  <div className="signup-form">
-    <div className="logo text-center">Sign up</div>
-    <Field name="username" type="text" component={FormInput} placeholder="Username" />
-    <Field name="firstname" type="text" component={FormInput} placeholder="Firstname" />
-    <Field name="lastname" type="text" component={FormInput} placeholder="Lastname" />
-    <Field name="email" type="email" component={FormInput} placeholder="Email" />
-    <Field name="password" type="password" component={FormInput} placeholder="Password" />
-  </div>
+const SignupForm = (props) => {
+  const handleFormSubmit = (values) => {
+    props.signup(values.username, values.firstname, values.lastname, values.email, values.password)
+  }
 
-export default reduxForm({
+  // eslint-disable-next-line react/prop-types
+  const FormInput = ({ input, type, placeholder }) =>
+    <div className="form-group">
+      <input className="form-control" {...input} type={type} placeholder={placeholder} />
+    </div>
+
+  return (
+    <div className="signup-form">
+      <div className="logo text-center">Sign up</div>
+      <form onSubmit={props.handleSubmit(handleFormSubmit)}>
+        <Field name="username" type="text" component={FormInput} placeholder="Username" />
+        <Field name="firstname" type="text" component={FormInput} placeholder="Firstname" />
+        <Field name="lastname" type="text" component={FormInput} placeholder="Lastname" />
+        <Field name="email" type="email" component={FormInput} placeholder="Email" />
+        <Field name="password" type="password" component={FormInput} placeholder="Password" />
+        <div className="form-group text-right">
+          <button type="submit" className="btn btn-primary">Sign up</button>
+        </div>
+      </form>
+    </div>
+  )
+}
+
+SignupForm.propTypes = {
+  signup: React.PropTypes.func.isRequired,
+  handleSubmit: React.PropTypes.func.isRequired,
+}
+
+const ReduxSignupForm = reduxForm({
   form: 'signup',
 })(SignupForm)
+
+export default connect(null, { signup })(ReduxSignupForm)
