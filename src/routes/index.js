81784f3568b4ebdf8505158096c9a33150cca8d4
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-30 12:23:27 +0700

    Add routes to Login and Signup pages

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 import Feed from './Feed'
 import User from './User'
+import Login from './Login'
+import Signup from './Signup'
 
 const routes = (
   <Route path="/">
     <IndexRoute component={Feed} />
+    <Route path="/login" component={Login} />
+    <Route path="/signup" component={Signup} />
     <Route path="/:username" component={User} />
   </Route>
 )
