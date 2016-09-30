920bf2e7dc85481eaeaacabd2f8c4ddc7741a72b
Supasate Choochaisri <supasate.c@gmail.com>
2016-09-30 15:43:03 +0700

    Add signup actions

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -1,3 +1,4 @@
+import { push } from 'redux-router'
 import types from './types'
 
 const loginInProgress = () => ({
@@ -38,10 +39,61 @@ const login = (username, password) => (dispatch) => {
   .catch(error => dispatch(loginFail(error)))
 }
 
+const signupInProgress = () => ({
+  type: types.SIGNUP_IN_PROGRESS,
+})
+
+const signupSuccess = (username, name) => ({
+  type: types.SIGNUP_SUCCESS,
+  payload: {
+    username,
+    name,
+  },
+})
+
+const signupFail = err => ({
+  type: types.SIGNUP_FAIL,
+  payload: err,
+  error: true,
+})
+
+const signup = (username, firstname, lastname, email, password) => (dispatch) => {
+  dispatch(signupInProgress())
+
+  fetch('http://localhost:3000/api/TwisterUsers', {
+    method: 'POST',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      username,
+      name: `${firstname} ${lastname}`,
+      email,
+      password,
+    }),
+  })
+  .then((response) => {
+    if (!response.ok) {
+      throw Error(response.statusText)
+    }
+    return response.json()
+  })
+  .then((authInfo) => {
+    dispatch(signupSuccess(authInfo.username, authInfo.name, authInfo.token))
+    dispatch(push('/login'))
+  })
+  .catch(error => dispatch(signupFail(error)))
+}
+
 
 export {
   login,
   loginInProgress,
   loginSuccess,
   loginFail,
+  signup,
+  signupInProgress,
+  signupSuccess,
+  signupFail,
 }
