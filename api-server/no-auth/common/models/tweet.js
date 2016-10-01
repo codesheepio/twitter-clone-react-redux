563668760760ed07971161d09aa03c989ba77bd6
Supasate Choochaisri <supasate.c@gmail.com>
2016-10-01 10:22:45 +0700

    Add homefeed api

--- api-server/no-auth/common/models/tweet.js
+++ api-server/no-auth/common/models/tweet.js
@@ -14,4 +14,42 @@ module.exports = function(Tweet) {
   Tweet.disableRemoteMethod('createChangeStream', true);
   Tweet.disableRemoteMethod('replaceOrCreate', true);
   Tweet.disableRemoteMethod('upsertWithWhere', true);
+
+  Tweet.homeFeed = Bluebird.promisify(function homeFeed(req, cb) {
+    var currentUser = req.accessToken && req.accessToken.userId;
+    var Follow = Tweet.app.models.Follow;
+    var followings = []
+    Follow.find({
+      where: {
+        username: currentUser,
+      },
+    })
+    .then(function(follows) {
+      return follows.map(follow => follow.followedUsername);
+    })
+    .then(function(followers) {
+      var usersForFeed = followers;
+      usersForFeed.push(currentUser);
+
+      return Tweet.find({
+        where: {
+          username: {
+            inq: usersForFeed,
+          },
+        },
+      })
+    })
+    .then(function(tweets) {
+      cb(null, tweets);
+    })
+    .catch(function(err) {
+      cb(err);
+    })
+  });
+
+  Tweet.remoteMethod('homeFeed', {
+    http: { path: '/homefeed', verb: 'get' },
+    accepts: { arg: 'req', type: 'object', required: true, http: { source: 'req' } },
+    returns: { arg: 'feed', type: ["object"] },
+  })
 };
